c522d1ef8353b1b24bf9fd4d6e7f9887e15ec380
yohanes1989 <yohanes1989@hotmail.com>
2016-10-04 13:13:06 +0700

    rename inputmask folder

--- resources/views/backend/master/form_template.blade.php
+++ resources/views/backend/master/form_template.blade.php
@@ -15,8 +15,8 @@
     <script src="{{ asset('backend/assets/template/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('backend/assets/template/global/plugins/moment.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('backend/assets/template/global/plugins/bootstrap-typeahead/bootstrap3-typeahead.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('backend/assets/template/global/plugins/bootstrap-maxlength/bootstrap-maxlength.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('backend/assets/scripts/plugins/inputmask/dist/min/jquery.inputmask.bundle.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('backend/assets/template/global/plugins/bootstrap-maxlength/bootstrap-maxlength.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('backend/assets/scripts/plugins/jquery-seo-url-master/jquery.seourl.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('backend/assets/scripts/plugins/html5sortable/html.sortable.min.js') }}" type="text/javascript"></script>
     <script src="{{ asset('backend/assets/template/global/plugins/jquery-multi-select/js/jquery.multi-select.js') }}" type="text/javascript"></script>
