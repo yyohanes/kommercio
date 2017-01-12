d6c692731c688c68942009c357ec076997904b1d
Yohanes <yohanes@webpresso.co.id>
2017-01-12 15:14:51 +0000

    rajaongkirinternasional bug

--- app/ShippingMethods/RajaOngkirInternational.php
+++ app/ShippingMethods/RajaOngkirInternational.php
@@ -73,60 +73,64 @@ class RajaOngkirInternational implements ShippingMethodInterface
             $destination = Country::find($order->shippingInformation->country_id);
         }
 
-        if($destination->iso_code != 'ID' && !empty($origin) && !empty($destination)){
-            //Call Raja Ongkir API
-            $client = new Client();
-            $res = $client->request('GET', KommercioAPIHelper::getAPIUrl().'/rajaongkir/rates/international', [
-                'http_errors' => false,
-                'query' =>  [
-                    'city_id' => $origin->master_id,
-                    'country_id' => $destination->master_id,
-                    'weight' => (int) $order->getTotalWeight(),
-                    'api_token' => KommercioAPIHelper::getAPIToken(),
-                ],
-            ]);
+        if(!empty($destination) && $destination->iso_code != 'ID' && !empty($origin) && !empty($destination)){
+            try{
+                //Call Raja Ongkir API
+                $client = new Client();
+                $res = $client->request('GET', KommercioAPIHelper::getAPIUrl().'/rajaongkir/rates/international', [
+                    'http_errors' => false,
+                    'query' =>  [
+                        'city_id' => $origin->master_id,
+                        'country_id' => $destination->master_id,
+                        'weight' => (int) $order->getTotalWeight(),
+                        'api_token' => KommercioAPIHelper::getAPIToken(),
+                    ],
+                ]);
 
-            if($res->getStatusCode() == 200) {
-                $body = $res->getBody();
+                if($res->getStatusCode() == 200) {
+                    $body = $res->getBody();
 
-                $results = json_decode($body);
+                    $results = json_decode($body);
 
-                if($results){
-                    $tiki = 0;
-                    $pos = 0;
+                    if($results){
+                        $tiki = 0;
+                        $pos = 0;
 
-                    foreach($results as $service){
-                        if(isset($methods[$service->code])){
-                            foreach($service->costs as $cost){
-                                if($service->code == 'tiki'){
-                                    if($cost->service == 'Paket'){
-                                        $tiki = $cost->cost;
-                                    }
-                                }elseif($service->code == 'pos'){
-                                    if($cost->service == 'EMS BARANG'){
-                                        $pos = $cost->cost;
+                        foreach($results as $service){
+                            if(isset($methods[$service->code])){
+                                foreach($service->costs as $cost){
+                                    if($service->code == 'tiki'){
+                                        if($cost->service == 'Paket'){
+                                            $tiki = $cost->cost;
+                                        }
+                                    }elseif($service->code == 'pos'){
+                                        if($cost->service == 'EMS BARANG'){
+                                            $pos = $cost->cost;
+                                        }
                                     }
                                 }
                             }
                         }
-                    }
 
-                    if($tiki){
-                        $return['tiki'] = $methods['tiki'];
-                        $return['tiki']['price'] = [
-                            'currency' => 'idr',
-                            'amount' => $tiki
-                        ];
-                    }
+                        if($tiki){
+                            $return['tiki'] = $methods['tiki'];
+                            $return['tiki']['price'] = [
+                                'currency' => 'idr',
+                                'amount' => $tiki
+                            ];
+                        }
 
-                    if($pos){
-                        $return['pos'] = $methods['pos'];
-                        $return['pos']['price'] = [
-                            'currency' => 'idr',
-                            'amount' => $pos
-                        ];
+                        if($pos){
+                            $return['pos'] = $methods['pos'];
+                            $return['pos']['price'] = [
+                                'currency' => 'idr',
+                                'amount' => $pos
+                            ];
+                        }
                     }
                 }
+            }catch (\Exception $e){
+
             }
         }
 
