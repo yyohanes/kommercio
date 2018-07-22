552e8fc377dc7d22ade4bd58ac6fbe4d6426e6b2
Yohanes <yohanes@irvinsaltedegg.com>
2018-07-22 22:09:40 +0800

    chore (page api): filter page content with shortcode

--- app/Http/Resources/Page/PageResource.php
+++ app/Http/Resources/Page/PageResource.php
@@ -3,6 +3,7 @@
 namespace Kommercio\Http\Resources\Page;
 
 use Illuminate\Http\Resources\Json\Resource;
+use Kommercio\Facades\Shortcode;
 use Kommercio\Http\Resources\Media\ImageCollection;
 use Kommercio\Models\CMS\Page;
 
@@ -16,7 +17,7 @@ class PageResource extends Resource {
             'id' => $page->id,
             'name' => $page->name,
             'slug' => $page->slug,
-            'body' => $page->body,
+            'body' => Shortcode::doShortcode($page->body),
             'images' => new ImageCollection($page->images),
             'metaTitle' => $page->meta_title,
             'metaDescription' => $page->meta_description,
