df07f1a6350b161ee91b172ff37ec83d269f0a87
Yohanes <yohanes@irvinsaltedegg.com>
2018-10-08 15:49:14 +0700

    chore (post): add sort by quer default param

--- app/Http/Controllers/Api/Frontend/Post/PostController.php
+++ app/Http/Controllers/Api/Frontend/Post/PostController.php
@@ -53,13 +53,12 @@ class PostController extends Controller {
             });
         }
 
-        if ($request->get('sort')) {
-            $sort = explode(':', $request->get('sort'));
-            $sortBy = $sort[0] ?? 'created_at';
-            $sortDirection = $sort[1] ?? 'desc';
+        $sortOption = $request->get('sort', 'created_at:desc');
+        $sort = explode(':', $sortOption);
+        $sortBy = $sort[0] ?? 'created_at';
+        $sortDirection = $sort[1] ?? 'desc';
 
-            $qb->orderBy($sortBy, $sortDirection);
-        }
+        $qb->orderBy($sortBy, $sortDirection);
 
         $posts = $qb->paginate($perPage);
         $posts->appends($request->except('page'));
