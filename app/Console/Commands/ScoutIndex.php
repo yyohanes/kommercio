0e0a71e31f9e141dd9cf130f1818f213be84262a
Yohanes <yohanes@irvinsaltedegg.com>
2019-01-07 18:15:42 +0700

    chore (index): add from to date to scout index command

--- app/Console/Commands/ScoutIndex.php
+++ app/Console/Commands/ScoutIndex.php
@@ -2,6 +2,7 @@
 
 namespace Kommercio\Console\Commands;
 
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Kommercio\Jobs\Index\OrderJob;
 use Kommercio\Models\Order\Order;
@@ -13,7 +14,7 @@ class ScoutIndex extends Command
      *
      * @var string
      */
-    protected $signature = 'scout-index';
+    protected $signature = 'scout-index {type} {--from_date=} {--to_date=}';
 
     /**
      * The console command description.
@@ -39,29 +40,47 @@ class ScoutIndex extends Command
      */
     public function handle()
     {
-        $type = $this->choice('Choose what data you want to index', ['Order']);
+        $type = $this->argument('type');
+        $fromDate = $this->option('from_date') ? new Carbon($this->option('from_date')) : null;
+        $toDate = $this->option('to_date') ? new Carbon($this->option('to_date')) : null;
 
-        switch($type) {
-            case 'Order':
-                $perBatch = 100;
-                $allRecords = Order::checkout()->count();
+        switch(strtolower($type)) {
+            case 'order':
+                $perBatch = 25;
+                $qb = Order::checkout();
+                if ($fromDate) {
+                    $qb->whereRaw('DATE_FORMAT(checkout_at, \'%Y-%m-%d\') >= ?', [$fromDate->format('Y-m-d')]);
+                }
+                if ($toDate) {
+                    $qb->whereRaw('DATE_FORMAT(checkout_at, \'%Y-%m-%d\') <= ?', [$toDate->format('Y-m-d')]);
+                }
+
+                $allRecords = $qb->count();
+                $this->info(sprintf('%d order (s) to be indexed', $allRecords));
+
                 $totalBatch = ceil($allRecords / $perBatch);
 
                 $bar = $this->output->createProgressBar($allRecords);
 
                 for ($i = 0; $i < $totalBatch; $i += 1) {
-                    $orders = Order::checkout()
+                    $orders = $qb
                         ->skip($i * $perBatch)
                         ->take($perBatch)
                         ->get();
 
                     foreach ($orders as $order) {
                         $job = new OrderJob($order);
-                        $job->handle();
+
+                        try {
+                            $job->handle();
+                        } catch (\Throwable $e) {
+                            $this->error(sprintf('Error indexing order #%s', $order->id));
+                        }
 
                         $bar->advance();
                     }
                 }
+                break;
             default:
                 $this->error('Unknown selection.');
                 break;
